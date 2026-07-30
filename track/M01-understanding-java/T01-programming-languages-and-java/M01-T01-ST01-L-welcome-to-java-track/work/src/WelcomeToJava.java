266f2c329c934b1e8c6b046ebacb61b3febf8ae0
Ankithasirimaddani <ankithasirim@gmail.com>
2026-07-30 12:56:15 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("Welcome to java track.");
+        System.out.println("i already know how to build programming logic.");
+        System.out.println("now i am learning how to express in java.");
     }
 }
