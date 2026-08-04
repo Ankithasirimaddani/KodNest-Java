e0ce8a611ea519d5f9152eb6594e2606a4bc3f09
Ankithasirimaddani <ankithasirim@gmail.com>
2026-08-04 11:32:26 +0530

    changesmade

---
+++ track/practice/week.java
@@ -0,0 +1,12 @@
+public class week {
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("THURSDAY");
+        System.out.print("FRIDAY");
+        System.out.println("SATURDAY");
+        System.out.println("SUNDAY");
+    }
+
+}
