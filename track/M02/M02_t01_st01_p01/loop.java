d8f10327676038b7f78273eac176f2f0e67169f5
Ankithasirimaddani <ankithasirim@gmail.com>
2026-08-05 12:21:19 +0530

    madechanges

---
+++ track/M02/M02_t01_st01_p01/loop.java
@@ -0,0 +1,9 @@
+package track.M02.M02_t01_st01_p01;
+
+public class loop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
