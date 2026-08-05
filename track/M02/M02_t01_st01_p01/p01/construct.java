31a27fa38be9f1040beb0fe0ef998734a740b1a9
Ankithasirimaddani <ankithasirim@gmail.com>
2026-08-05 11:18:23 +0530

    changes

---
+++ track/M02/M02_t01_st01_p01/p01/construct.java
@@ -0,0 +1,30 @@
+package track.M02.M02_t01_st01_p01.p01;
+
+import java.util.Scanner;
+
+public class construct {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num = sc.nextInt();
+        switch (num) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            default:
+                break;
+        }
+    }
+}
