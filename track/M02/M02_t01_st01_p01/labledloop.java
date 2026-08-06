75e906e116fc8e46c59d275be352375cae1afd58
Ankithasirimaddani <ankithasirim@gmail.com>
2026-08-06 13:00:25 +0530

    madechanges

--- track/M02/M02_t01_st01_p01/labledloop.java
+++ track/M02/M02_t01_st01_p01/labledloop.java
@@ -1,6 +1,6 @@
 package track.M02.M02_t01_st01_p01;
 
-public class NestedPgm2 {
+public class labledloop {
     public static void main(String[] args) {
         outer: for (int i = 1; i <= 3; i++) {
             inner: for (int j = 1; j <= 4; j++) {
