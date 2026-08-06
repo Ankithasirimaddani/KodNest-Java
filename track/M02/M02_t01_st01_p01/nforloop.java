b85400117e9be4bb52b7ddb6b6550848716c7d3f
Ankithasirimaddani <ankithasirim@gmail.com>
2026-08-06 11:34:51 +0530

    madechanges

---
+++ track/M02/M02_t01_st01_p01/nforloop.java
@@ -0,0 +1,13 @@
+package track.M02.M02_t01_st01_p01;
+
+public class nforloop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.println(j);
+            }
+            System.out.println();
+        }
+    }
+
+}
