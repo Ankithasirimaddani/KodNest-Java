487223962f415e33b0e97341d1015863bbc2a7b8
Ankithasirimaddani <ankithasirim@gmail.com>
2026-08-06 12:25:27 +0530

    madechanges

---
+++ track/M02/M02_t01_st01_p01/Break.java
@@ -0,0 +1,12 @@
+package track.M02.M02_t01_st01_p01;
+
+public class Break {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            if (i == 5) {
+                break;
+            }
+        }
+    }
+}
