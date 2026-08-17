a88f1039554a2d1132d7185ea5673721c97f3c36
Ankithasirimaddani <ankithasirim@gmail.com>
2026-08-17 12:50:44 +0530

    Arrayday1

---
+++ track/Array/pgm1.java
@@ -0,0 +1,23 @@
+package track.Array;
+
+import java.util.Scanner;
+
+public class pgm1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a[] = new int[5];
+        System.out.println("Enter the values");
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = scan.nextInt();
+        }
+
+        System.out.println("Array Elemnts are:");
+
+        for (int i = 0; i <= a.length - 1; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+
+}
