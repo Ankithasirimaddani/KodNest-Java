a345aaae1190b0dcb59c593eec7a2ebe86bd86e1
Ankithasirimaddani <ankithasirim@gmail.com>
2026-09-09 16:47:14 +0530

    String

---
+++ track/String/StringComparision.java
@@ -0,0 +1,12 @@
+package track.String;
+
+public class StringComparision {
+    public static void main(String[] args) {
+        String s1 = "java";
+        String res = s1.toUpperCase();
+
+        System.out.println(s1);
+        System.out.println(res);
+    }
+
+}
---
+++ track/String/methods.java
@@ -0,0 +1,9 @@
+package track.String;
+
+public class methods {
+    public static void main(String[] args) {
+        String s1 = "Ankitha Maddani";
+        
+    
+    
+}
