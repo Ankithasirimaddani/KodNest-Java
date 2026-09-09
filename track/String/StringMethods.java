e382398baea353e9ec88fc0e8aacb2c2507d8bbb
Ankithasirimaddani <ankithasirim@gmail.com>
2026-09-09 16:50:02 +0530

    string

---
+++ track/String/StringMethods.java
@@ -0,0 +1,11 @@
+package track.String;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String s1 = "Ankitha Maddani";
+        String s2 = "Siri";
+        String res = s1.concat(s2);
+        System.out.println(res);
+    }
+
+}
